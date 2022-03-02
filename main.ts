0e0354ccec699a0336d6ceb6f96f34f9993bbaff
Rob Wilson <rob@rwilson.org.uk>
2022-03-02 10:51:29 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,10 @@ let date = ""
 let time = ""
 let dateTime = ""
 let stringIn = ""
+let month = ""
+let year = ""
+let minute = ""
+let hour = ""
 let command = ""
 function readTime () {
     date = "" + DS3231.date() + "/" + DS3231.month() + "/" + DS3231.year()
@@ -13,36 +17,50 @@ input.onButtonPressed(Button.A, function () {
     basic.showString(dateTime)
     serial.writeLine(dateTime)
 })
+function setDate () {
+    // the first 2 characters after command
+    date = stringIn.substr(2, 2)
+    // the next 2 characters
+    month = stringIn.substr(4, 2)
+    // the last 4 characters
+    year = stringIn.substr(6, 4)
+    DS3231.dateTime(
+    parseFloat(year),
+    parseFloat(month),
+    parseFloat(date),
+    DS3231.day(),
+    DS3231.hour(),
+    DS3231.minute(),
+    0
+    )
+    basic.showNumber(DS3231.date())
+    basic.showNumber(DS3231.month())
+    basic.showNumber(DS3231.year())
+}
+function setTime () {
+    // the first 2 characters after command
+    minute = stringIn.substr(2, 2)
+    // the next 2 characters command
+    hour = stringIn.substr(4, 2)
+    DS3231.dateTime(
+    DS3231.year(),
+    DS3231.month(),
+    DS3231.date(),
+    DS3231.day(),
+    parseFloat(hour),
+    parseFloat(minute),
+    0
+    )
+    basic.showNumber(DS3231.hour())
+    basic.showNumber(DS3231.minute())
+}
 serial.onDataReceived(serial.delimiters(Delimiters.CarriageReturn), function () {
     stringIn = serial.readUntil(serial.delimiters(Delimiters.CarriageReturn))
-    if (stringIn.length >= 2) {
-        command = stringIn.substr(0, 2)
-        if (command == "st") {
-            DS3231.dateTime(
-            DS3231.year(),
-            DS3231.month(),
-            DS3231.date(),
-            DS3231.day(),
-            parseFloat(stringIn.substr(2, 2)),
-            parseFloat(stringIn.substr(4, 2)),
-            0
-            )
-            basic.showNumber(DS3231.hour())
-            basic.showNumber(DS3231.minute())
-        }
-        if (command == "sd") {
-            DS3231.dateTime(
-            parseFloat(stringIn.substr(6, 4)),
-            parseFloat(stringIn.substr(4, 2)),
-            parseFloat(stringIn.substr(2, 2)),
-            DS3231.day(),
-            DS3231.hour(),
-            DS3231.minute(),
-            0
-            )
-            basic.showNumber(DS3231.date())
-            basic.showNumber(DS3231.month())
-            basic.showNumber(DS3231.year())
-        }
+    command = stringIn.substr(0, 2)
+    if (command == "st") {
+        setTime()
+    }
+    if (command == "sd") {
+        setDate()
     }
 })
